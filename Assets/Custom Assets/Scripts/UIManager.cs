fed5798fab99479cd0478380d1fe0726f04344ba
blight15a <blight1115.dev@gmail.com>
2023-12-26 19:40:36 +0900

    fixed model and implemented main vr functionalities

---
+++ Assets/Custom Assets/Scripts/UIManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnSelect_Button()
+    {
+        print("OnSelect_Button has been called");
+    }
+
+    public void OnHover_Button()
+    {
+        print("OnHover_Button has been called");
+    }
+
+}
